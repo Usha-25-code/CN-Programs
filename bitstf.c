a0b18c4331e7784f14ff8a8998422b0773fe07ce
M.BalaUshaSree <balaushasree29@gmail.com>
2026-07-15 10:27:04 +0530

    Add files via upload

---
+++ bitstf.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main(){
+        int a[100],b[100];
+        int i,j=0,n;
+        int count=0;
+        printf("enter frame  length:\n");
+        scanf("%d",&n);
+        printf("enter input's frame");
+        for(i=0;i<n;i++){
+            scanf("%d",&a[i]);
+        }
+        for(i=0;i<n;i++){
+           b[j++]=a[i];
+           if(a[i]==1){
+            count++;
+            if(count==5){
+             b[j++]=0;
+             count=0;
+            }
+           }
+           else{
+            count=0;
+           }
+          }
+           printf("\n after bit stuffing:\n");
+           for(i=0;i<j;i++){
+            printf("%d",b[i]);
+           }
+         printf("\n");
+}
