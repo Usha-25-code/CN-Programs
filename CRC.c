37f7a020134f41096365113441050517ceb08e03
M.BalaUshaSree <balaushasree29@gmail.com>
2026-07-29 10:24:33 +0530

    Add files via upload

---
+++ CRC.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main() {
+    int data[100], div[20], temp[100];
+    int datalen = 0, divlen = 0, i, j;
+    char ch;
+
+    printf("Enter the data: ");
+    while ((ch = getchar()) != '\n') {
+        if (ch == '1' || ch == '0') {
+            data[datalen++] = ch - '0';
+        }
+    }
+
+    printf("Enter the divisor: ");
+    while ((ch = getchar()) != '\n') {
+        if (ch == '1' || ch == '0') {
+            div[divlen++] = ch - '0';
+        }
+    }
+
+    for (i = 0; i < datalen; i++) {
+        temp[i] = data[i];
+    }
+
+    for (i = 0; i < divlen - 1; i++) {
+        temp[datalen + i] = 0;
+    }
+    int totallen = datalen + divlen - 1;
+    for (i = 0; i < datalen; i++) {
+        if (temp[i] == 1) {
+            for (j = 0; j < divlen; j++) {
+                temp[i + j] = temp[i + j] ^ div[j];
+            }
+        }
+    }
+    for (i = 0; i < divlen - 1; i++) {
+        data[datalen + i] = temp[datalen + i];
+    }
+    printf("Transmitted data (Data + CRC): ");
+    for (i = 0; i < totallen; i++) {
+        printf("%d", data[i]);
+    }
+    printf("\n");
+    return 0;
+}
+
